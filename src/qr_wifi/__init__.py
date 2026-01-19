a5475278014addc9f2705704c49b3ab84680538c
Unkn0wnN4m3 <87839850+Unkn0wnN4m3@users.noreply.github.com>
2026-01-19 00:51:35 -0600

    refactor: use uv instead of pipenv

---
+++ src/qr_wifi/__init__.py
@@ -0,0 +1,33 @@
+import argparse
+
+import qrcode
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(description="Make a QR from WiFi data")
+
+    parser.add_argument("--ssid", dest="SSID", required=True)
+    parser.add_argument(
+        "--security",
+        dest="SECURITY",
+        required=True,
+        choices=["WPA", "WEP", "WPA2"],
+    )
+    parser.add_argument("--password", dest="PASSWORD", required=True)
+
+    args = parser.parse_args()
+
+    # QR initialisation
+    qr = qrcode.QRCode(error_correction=qrcode.ERROR_CORRECT_L, box_size=10, border=4)
+
+    ssid, security, password = args.SSID, args.SECURITY, args.PASSWORD
+    data = f"WIFI:S:{ssid};T:{security};P:{password};;"
+    print(data)
+    qr.add_data(data)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save(f"{ssid}.png")
+
+
+if __name__ == "__main__":
+    main()
